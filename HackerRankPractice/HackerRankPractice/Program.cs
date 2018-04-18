6fbf2279066092a13bb78d230aa3562cabfb9d21
Britini Malloy (WTIA Workforce Institute) <v-brmall@microsoft.com>
2018-04-18 09:35:57 -0700

    initial code commit

---
+++ HackerRankPractice/HackerRankPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //my code will go here
+        }
+    }
+}
